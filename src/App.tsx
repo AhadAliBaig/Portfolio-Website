56e895e98da19d1f9620140c92c95c4505ea0003
Ahad Baig <ahadalibaig689@gmail.com>
2026-01-05 02:29:05 -0800

    main working

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,33 @@ import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import LiquidEther from './components/LiquidEther/LiquidEther'
 
 function App() {
   return (
     <div className="scroll-container">
-     <Header />
-     <Hero />
-     <About />
+      <div className="liquid-background">
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
+      <Header />
+      <Hero />
+      <About />
     </div>
   )
 }
