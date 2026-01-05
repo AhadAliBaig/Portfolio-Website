6ea5b3a9d01c3a68a8a0de92901a1c4029e7f67a
Ahad Baig <ahadalibaig689@gmail.com>
2026-01-05 02:37:15 -0800

    splash cursor added

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,12 @@ import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
-import LiquidEther from './components/LiquidEther/LiquidEther'
+import SplashCursor from './components/SplashCursor'
 
 function App() {
   return (
     <div className="scroll-container">
-      <div className="liquid-background">
-        <LiquidEther
-          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
-        />
-      </div>
+      <SplashCursor />
       <Header />
       <Hero />
       <About />
