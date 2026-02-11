81ea083ab1ef5896c8097eb8782debfbc7643a21
Ahad Baig <ahadalibaig689@gmail.com>
2026-02-11 08:43:36 -0800

    removed unused component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import TextType from '../TextType';
 import PixelSnow from '../PixelSnow';
 import ScrollAnimation from '../ScrollAnimation';
 import ASCIIText from '../ASCIIText';
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect} from 'react';
 
 function Hero() {
   // Detect if device is mobile/tablet
